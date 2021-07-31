bc58152dd542cd835ee4a7a786d772ca5978a998
Simon D.A. Thomas <sdat2@cam.ac.uk>
2021-07-31 16:35:12 +0100

    ext: adding read in data

--- src/read_in_data.py
+++ src/read_in_data.py
@@ -3,16 +3,17 @@
 python src/read_in_data.py
 """
 import os
+from typing import Tuple
 import numpy as np
 import pandas as pd
 import xarray as xr
 from src.constants import SRC_PATH
 
 
-print(pd.__version__)
 output_dir = os.path.join(SRC_PATH, "output_init")
 
-def get_param(name="hurr.in"):
+
+def get_param(name: str = "hurr.in") -> dict:
     param_d = {}
     with open(os.path.join(output_dir, name), "r") as file:
         for line in file:
@@ -32,14 +33,13 @@ def get_param(name="hurr.in"):
     param_d["ETIME"] = float(param_d["ETIME"])
     return param_d
 
-def get_s(name="s.in"):
+def get_s(name: str = "s.in") -> xr.Dataset:
     param_d_loc = {}
     s_l = []
     with open(os.path.join(output_dir, name), "r") as file:
         for line in file:
             el = [x for x in [x.strip(" ").strip("\n") for x in line.split(" ")] if x != ""]
             if len(el) >= 3:
-                print(el)
                 param_d_loc[el[0]] = [el[1], el[2]]
                 s_l.append(el)
     units = []
@@ -65,7 +65,16 @@ def get_s(name="s.in"):
     return ds
 
 
-def get_tab_array(name="time", delim="\t"):
+def get_tab_array(name="time", delim="\t") -> np.ndarray:
+    """Get the arrays at a particular level.
+
+    Args:
+        name (str, optional): Name. Defaults to "time".
+        delim (str, optional): Delimiter. Defaults to "\t".
+
+    Returns:
+        np.ndarray: numpy.
+    """
     out_lol = []
     with open(os.path.join(output_dir, name), "r") as file:
         for line in file:
@@ -74,7 +83,8 @@ def get_tab_array(name="time", delim="\t"):
             out_lol.append(el)
     return np.array(out_lol)
 
-def get_outputs(tim="05"):
+
+def get_outputs(tim: str="05") -> Tuple[dict, dict]:
     di_a = {}
     for key in ["radius", "rgraph", "zgraph", "time", "vmaxz", "vthov", "uthov", "vbhov", "ubhov", "thehov"]:
         di_a[key] = get_tab_array(name=key + ".out", delim=" ")
@@ -83,6 +93,42 @@ def get_outputs(tim="05"):
         ti_a[key] = get_tab_array(name=key + tim + ".out", delim=" ")
 
     return di_a, ti_a
+
+
+def tdsts():
+    def make_datasets(d2):
+        da_out = []
+        da_out2 = []
+        for key in d2:
+            ds = xr.Dataset(
+                data_vars={
+                    key: (["y", "x"], d2[key].astype("float32")),
+                },
+            )
+            ds[key].attrs["long_name"] =  key
+            def get_range(npa):
+                return min(npa), max(npa)
+
+            if np.all((0, 265) == get_range(ds[key]["x"].values)):
+                da_out.append(ds[key])
+            else:
+                da_out2.append(ds[key].rename({"x":"y", "y": "x"}))
+        return xr.merge(da_out), xr.merge(da_out)
+
+    tim_l = ["0"+ str(i) for i in range(1, 10)]
+    # tim_l[0] = ""
+    ds1_l = []
+    ds2_l = []
+    for tim in tim_l:
+        ti_a = {}
+        for key in ["ucon", "vcon", "wcon", "pcon", "tfcon", "qcon", "tescon", "tecon", "liqcon"]:
+            ti_a[key] = get_tab_array(name=key + tim + ".out", delim=" ")
+        ds1, ds2 = make_datasets(ti_a)
+        ds1_l.append(ds1)
+        ds2_l.append(ds2)
+
+    return xr.concat(ds1_l, "T"), xr.concat(ds2_l, "T")
+    # return ds1_l, ds2_l
 
 
 """"
@@ -97,6 +143,7 @@ print(get_tab_array(name="liqcon" + tim + ".out", delim=" "))
 """
 
 if __name__ == "__main__":
+    # python src/read_in_data.py
     time = 5
     router = 4
     di_a, ti_a = get_outputs()
